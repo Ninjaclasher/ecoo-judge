b8aaa73422cb5a5ad433058646c52bd4aacefed0
root <AvidDev@protonmail.com>
2020-04-09 04:29:42 +0000

    Swithc to only selenium

--- judge/management/commands/render_pdf.py
+++ judge/management/commands/render_pdf.py
@@ -8,7 +8,7 @@ from django.template.loader import get_template
 from django.utils import translation
 
 from judge.models import Problem, ProblemTranslation
-from judge.pdf_problems import DefaultPdfMaker, PhantomJSPdfMaker, PuppeteerPDFRender, SlimerJSPdfMaker
+from judge.pdf_problems import DefaultPdfMaker, SeleniumPDFRender
 
 
 class Command(BaseCommand):
@@ -19,11 +19,7 @@ class Command(BaseCommand):
         parser.add_argument('directory', nargs='?', help='directory to store temporaries')
         parser.add_argument('-l', '--language', default=settings.LANGUAGE_CODE,
                             help='language to render PDF in')
-        parser.add_argument('-p', '--phantomjs', action='store_const', const=PhantomJSPdfMaker,
-                            default=DefaultPdfMaker, dest='engine')
-        parser.add_argument('-s', '--slimerjs', action='store_const', const=SlimerJSPdfMaker, dest='engine')
-        parser.add_argument('-c', '--chrome', '--puppeteer', action='store_const',
-                            const=PuppeteerPDFRender, dest='engine')
+        parser.add_argument('-S', '--selenium', action='store_const', const=SeleniumPDFRender, dest='engine')
 
     def handle(self, *args, **options):
         try:
